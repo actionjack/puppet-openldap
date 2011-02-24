cfcda9aaa3ee94ff5f687b4c140f83f94a2b053a
actionjack <martin@uncommonsense-uk.com>
2011-02-24 10:52:47 +0000

    First Commit

---
+++ manifests/init.pp
@@ -0,0 +1,17 @@
+# Class: openldap
+#
+# This module manages openldap
+#
+# Parameters:
+#
+# Actions:
+#
+# Requires:
+#
+# Sample Usage:
+#
+# [Remember: No empty lines between comments and class definition]
+class openldap {
+
+
+}
